db578e3764a13557ad618e931a94b4f1969ad073
Mario Cagalj <mcagalj@fesb.hr>
2024-10-18 15:34:29 +0200

    Refactor home page component to add top level navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,36 @@
+// We hardcode pages here, but you could get this information from some external source (e.g. CMS, DB, config file, etc).
+const pages = [
+  { title: "Home", path: "/" },
+  {
+    title: "Showcase",
+    path: "/showcase",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "About us",
+    path: "/about",
+  },
+  {
+    title: "Contact us",
+    path: "/contact",
+  },
+];
+
+function processPage(page, index) {
+  return (
+    <li key={index}>
+      <a href={page.path}>{page.title}</a>
+    </li>
+  );
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
+      <ul className="flex space-x-4 mb-4">{pages.map(processPage)}</ul>
       <h1 className="text-6xl font-extrabold tracking-tight">Home page</h1>
     </main>
   );
