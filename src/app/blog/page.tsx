11ed67f967fb892f5380b18fd8179bba378b3787
Mario Cagalj <mcagalj@fesb.hr>
2024-10-24 11:26:03 +0200

    Refactor BlogPage into BlogPage index page to display links to blog posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,26 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Blog",
 };
 
+// This is typically fetched from an API
+const posts = [12, 3, 56, 7, 89];
+
+function processPost(id: number) {
+  return (
+    <li>
+      <Link href={`/blog/${id}`}>Post {id}</Link>
+    </li>
+  );
+}
+
 export default function BlogPage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
       <h1 className="text-6xl font-extrabold tracking-tight">Blog</h1>
+      <ul>{posts.map(processPost)}</ul>
     </main>
   );
 }
