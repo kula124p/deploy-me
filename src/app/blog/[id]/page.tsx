2b8709d98152ec61604e34f240b11114462d9206
Mario Cagalj <mcagalj@fesb.hr>
2024-10-24 11:12:28 +0200

    Refactor BlogPost component to use TypeScript and destructure props object

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,6 +1,8 @@
-export default function BlogPost(props) {
-  console.log(props);
-  const params = props.params;
+type BlogPostProps = {
+  params: { id: string };
+};
+
+export default function BlogPost({ params }: BlogPostProps) {
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
       <h1 className="text-6xl font-extrabold tracking-tight">
