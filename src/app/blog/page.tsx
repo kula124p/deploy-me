7aad34fe85bc431f0ecdd36524afb7e21cefdd8e
Mario Cagalj <mcagalj@fesb.hr>
2024-10-24 11:52:03 +0200

    Refactor BlogPage component to fetch and display (fake) blog posts from API

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,26 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
 export const metadata: Metadata = {
   title: "Blog",
 };
 
-// This is typically fetched from an API
-const posts = [12, 3, 56, 7, 89];
+const BASE_API_URL = "https://jsonplaceholder.typicode.com";
 
-function processPost(id: number) {
+async function getPosts(): Promise<Post[]> {
+  const data = await fetch(`${BASE_API_URL}/posts`);
+  return data.json();
+}
+
+function processPost(post: Post) {
+  const { id, title } = post;
   return (
     <li key={id} className="mb-4">
       <Link
@@ -16,7 +28,7 @@ function processPost(id: number) {
         className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 transition-colors duration-200"
       >
         <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-          Post {id}
+          Post {id}: {title}
         </h2>
         <p className="font-normal text-gray-700">
           Click to read more about this fascinating topic...
@@ -26,7 +38,8 @@ function processPost(id: number) {
   );
 }
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  const posts = await getPosts();
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
       <h1 className="text-6xl font-extrabold tracking-tight mb-10">Blog</h1>
