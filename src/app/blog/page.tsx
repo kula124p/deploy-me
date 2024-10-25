4376e175345fe762b51f2eb8c1d9fcacf7ef2d8a
Mario Cagalj <mcagalj@fesb.hr>
2024-10-25 11:22:49 +0200

    Refactor BlogPage component to include pagination and display page number

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,15 +8,31 @@ export type Post = {
   body: string;
 };
 
+type BlogPageProps = {
+  searchParams: { page: string };
+};
+
 export const metadata: Metadata = {
   title: "Blog",
 };
 
 export const BASE_API_URL = "https://jsonplaceholder.typicode.com";
+const PAGE_SIZE = 8;
 
 async function getPosts(): Promise<Post[]> {
   const data = await fetch(`${BASE_API_URL}/posts`);
   return data.json();
+}
+
+// Get the total number of posts; please note this feature is API specific.
+async function getPostsCount(): Promise<number> {
+  // https://jsonplaceholder.typicode.com/posts/?_start=5&_limit=8
+  const data = await fetch(`${BASE_API_URL}/posts/?_limit=1`, {
+    method: "HEAD",
+  });
+  let count: string | number = data.headers.get("x-total-count") || "1";
+  count = parseInt(count, 10);
+  return count;
 }
 
 function processPost(post: Post) {
@@ -38,11 +54,33 @@ function processPost(post: Post) {
   );
 }
 
-export default async function BlogPage() {
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+  const postsCount = await getPostsCount();
+  const pagesCount = Math.ceil(postsCount / PAGE_SIZE);
+  // Ensure the page number is a positive integer.
+  const currentPage = Math.min(
+    /^[1-9][0-9]*$/.test(searchParams.page) ? Number(searchParams.page) : 1,
+    pagesCount
+  );
+  const _start = (currentPage - 1) * PAGE_SIZE;
+  const _limit = PAGE_SIZE;
+
+  console.log({ searchParams });
+  console.log({ pagesCount, currentPage, _start, _limit });
+
   const posts = await getPosts();
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
       <h1 className="text-6xl font-extrabold tracking-tight mb-10">Blog</h1>
+      <div className="w-full max-w-2xl mb-6">
+        <div className="bg-white rounded-lg border border-gray-200 shadow-sm p-4 text-center">
+          <p className="text-gray-700">
+            Page{" "}
+            <span className="font-semibold text-gray-900">{currentPage}</span>{" "}
+            of <span className="font-semibold text-gray-900">{pagesCount}</span>
+          </p>
+        </div>
+      </div>
       <ul className="w-full max-w-2xl space-y-4">{posts.map(processPost)}</ul>
     </main>
   );
