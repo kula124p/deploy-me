c3d28b3aa3d1de14aa7b666af199e54a3031f720
Mario Cagalj <mcagalj@fesb.hr>
2024-11-11 12:49:28 +0100

    Revamp home page content and introduce Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import Button from "@/components/ui/button";
+
+// Here we hardcode the content of the home page, but in a real-world scenario, this content would be fetched from a CMS or a DB.
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-6xl font-lato font-black tracking-tight">
-        Home page
-      </h1>
+    <main className="container space-y-6 pt-4">
+      <div className="space-y-1">
+        <h1 className="text-4xl font-lato font-black tracking-tight text-balance">
+          Where Vision Meets Innovation
+        </h1>
+        <h4 className="text-xl text-brand-text-weak tracking-tight">
+          Entrust us with your digital appearance
+        </h4>
+      </div>
+      <p className="leading-6">
+        We are a full-service digital agency that specializes in web design,
+        development, and digital marketing. We create digital experiences that
+        are unique to your brand and help you achieve your goals.
+      </p>
+      <Button secondary>Book a meeting</Button>
     </main>
   );
 }
